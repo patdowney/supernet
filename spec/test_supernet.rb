f66be8c9504acf6bee1aa09b89a70ff64608525c
Pat Downey <pat.downey@gmail.com>
2012-05-29 11:31:51 +0100

    Initial commit of supernet helper

---
+++ spec/test_supernet.rb
@@ -0,0 +1,162 @@
+require 'spec_helper'
+require 'supernet'
+require 'ipaddress'
+
+describe "SuperNet" do
+  it "should list available networks" do
+    super_net = SuperNet.new( "192.168.0.0/24" )
+    available_nets = super_net.available(25)
+    available_nets.length.should == 2
+  end
+  
+  it "should be able to pre-allocate an ip address" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    super_net.pre_allocate("192.168.0.16/28")
+    super_net.allocated?("192.168.0.16/28").should be_true
+   end
+
+  it "should not allocate the same network more than once" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.16/28").should be_true
+
+    expect { super_net.pre_allocate("192.168.0.16/28") }.to raise_error("192.168.0.16/28 already allocated.")
+  end
+
+  it "should return false if a network hasn't been allocated yet" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.allocated?("192.168.0.32/28").should be_false
+  end
+
+  it "should raise an error if requested network is larger than supernet" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    expect { super_net.pre_allocate("192.168.0.0/23") }.to raise_error( "192.168.0.0/23 is larger than supernet 192.168.0.0/24." )
+  end
+ 
+  it "overlap? should return true if requested net would overlap an already allocated smaller nets" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.16/29").should be_true
+    
+    net = IPAddress.parse("192.168.0.16/28")
+    super_net.net_overlap?(net).should be_true
+
+    net = IPAddress.parse("192.168.0.16/30")
+    super_net.net_overlap?(net).should be_true
+  end
+
+  it "overlap? should return truee if requested net would overlap an already allocated larger net" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.16/29").should be_true
+    
+    net = IPAddress.parse("192.168.0.20/28")
+    super_net.net_overlap?(net).should be_true
+  end
+
+  it "overlap? should return false if requested net would not overlap an already allocated net" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.16/29").should be_true
+    
+    net = IPAddress.parse("192.168.0.20/29")
+    super_net.net_overlap?(net).should be_false
+  end
+
+  it "should not allocate two networks that overlap" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.16/29").should be_true
+    expect { super_net.pre_allocate("192.168.0.16/30") }.to raise_error("192.168.0.16/30 already allocated.")
+  end
+
+  it "should allocate a network with the requested netmask" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    new_net = super_net.allocate(25)
+
+    new_net.prefix.to_i.should  == 25
+    new_net.network.address.should == "192.168.0.0"
+  end
+
+  it "should allocate consecutive network with the requested netmask" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    new_net = super_net.allocate(25)
+    new_net.should == IPAddress.parse("192.168.0.0/25")
+
+    new_net_2 = super_net.allocate(25)
+    new_net_2.should == IPAddress.parse("192.168.0.128/25")
+   end
+
+  it "should error if all networks have been allocated" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    super_net.allocate(25)
+    super_net.allocate(25)
+    expect {super_net.allocate(25)}.to raise_error( "Unable to allocate /25 network." )
+   end
+
+  it "should allocate networks of different sizes" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    super_net.allocate(25)
+    new_net = super_net.allocate(30)
+    new_net.should == IPAddress.parse("192.168.0.128/30")
+  end
+
+  it "should be able to allocate the entire supernet as a subnet" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    new_net = super_net.allocate(24)
+    new_net.should == IPAddress.parse("192.168.0.0/24")
+  end
+
+# the same as the one above except with the larger network first
+  it "should allocate networks of different sizes" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    super_net.allocate(30)
+    new_net = super_net.allocate(25)
+    new_net.should == IPAddress.parse("192.168.0.128/25")
+  end
+
+  it "should allocate a network big enough to contain a number of hosts" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    net_new = super_net.allocate_for_hosts(2)
+    net_new.should == IPAddress.parse("192.168.0.0/30")
+  end
+
+  it "should work out an appropriate netmask for a given number of hosts" do
+    super_net = SuperNet.new("192.168.0.0/24")
+
+    super_net.netmask_for_hosts(2).should == 30
+    super_net.netmask_for_hosts(3).should == 29
+    super_net.netmask_for_hosts(4).should == 29
+    super_net.netmask_for_hosts(253).should == 24
+    super_net.netmask_for_hosts(257).should == 23
+    
+    expect { super_net.netmask_for_hosts(0) }.to raise_error("Number of hosts requested is out of bounds (should be between 1 and 4294967296")
+  end
+
+  # this test against a silly mistake that I don't want to happen again.
+  it "should test against all previous allocations", :fail=> true do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.0/28").should be_true
+    super_net.pre_allocate("192.168.0.16/30").should be_true
+    expect { super_net.pre_allocate("192.168.0.0/30") }.to raise_error("192.168.0.0/30 already allocated.")
+  end
+
+  it "should overlap with these networks that differ only by netmask" do
+    super_net = SuperNet.new("192.168.0.0/24")
+    super_net.pre_allocate("192.168.0.0/28").should be_true
+    
+    net = IPAddress.parse("192.168.0.0/30")
+    super_net.net_overlap?(net).should be_true
+
+    net = IPAddress.parse("192.168.0.0/28")
+    super_net.net_overlap?(net).should be_true
+
+    net = IPAddress.parse("192.168.0.0/29")
+    super_net.net_overlap?(net).should be_true
+  end
+
+end
+
---
+++ supernet.rb
@@ -0,0 +1,115 @@
+require 'ipaddress'
+require 'set'
+
+class SuperNet
+  attr_reader :network
+  attr_reader :allocated
+
+  def initialize(network_string)
+    @network = IPAddress.parse(network_string)
+    @allocated = Set.new
+  end
+
+  def all(netmask)
+    Set.new @network.subnet(netmask)
+  end
+
+  def available(netmask)
+    self.all(netmask).subtract @allocated
+  end
+
+  def pre_allocate_net(net)
+#    if @allocated.include?(net)
+    if net_overlap?(net)
+      raise "#{net.address}/#{net.prefix} already allocated."
+    else
+      if net.prefix >= @network.prefix
+        @allocated.add net
+      else
+        raise "#{net.address}/#{net.prefix} is larger than supernet #{@network.address}/#{@network.prefix}."
+      end
+    end
+    return net
+  end
+
+  def pre_allocate(network)
+    net = IPAddress.parse(network)
+    return pre_allocate_net(net)
+  end
+
+  def net_range_overlap?(net_a,net_b)
+    outside = (net_a.last < net_b.first) || (net_a.first > net_b.last)
+    return ! outside
+  end
+
+  def net_prefix_overlap?(net_a,net_b)
+    if net_a.prefix < net_b.prefix
+      return net_a.subnet(net_b.prefix).include?(net_b)
+    else
+      return net_b.subnet(net_a.prefix).include?(net_a)
+    end
+  end
+
+  def net_overlap?(net_a)
+    overlap_found = false
+    @allocated.each do |net_b|
+      overlap_found ||= net_prefix_overlap?(net_a, net_b)
+      #overlap_found ||= net_range_overlap?(net_a, net_b)
+    end
+    overlap_found
+  end
+
+  def allocated?(network)
+    net = IPAddress.parse(network)
+    net_allocated?(net)
+  end
+
+  def net_allocated?(net)
+    @allocated.include? net
+  end
+
+  def allocate(netmask)
+    all_subnets = @network.subnet(netmask)
+    allocated_net = nil
+    all_subnets.each do |net|
+      if not net_allocated?(net)
+        if not net_overlap?(net)
+          allocated_net = net
+          break
+        end
+      end
+    end
+
+    if allocated_net.nil?
+      raise "Unable to allocate /#{netmask} network."
+    end
+
+    return pre_allocate_net(allocated_net)
+  end
+
+  def allocate_for_hosts(num_hosts)
+    netmask = netmask_for_hosts(num_hosts)
+    allocate(netmask) 
+  end
+
+  def max_hosts
+    2**max_bits
+  end
+
+  def max_bits
+    32
+  end
+
+  def netmask_for_hosts(num_hosts)
+    if num_hosts > max_hosts or num_hosts < 1
+      raise "Number of hosts requested is out of bounds (should be between 1 and #{max_hosts}"
+    end
+
+    num_hosts_with_net_and_bcast = num_hosts + 2
+
+    host_bits = ( (0..max_bits).select {|i| num_hosts_with_net_and_bcast <= 2**i} .first )
+
+    netmask = max_bits - host_bits
+  end
+
+end
